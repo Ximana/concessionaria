6eb710216299c0982c7d998dfa78ce782a4bf11a
Paulo Ximana <pauloximana@gmail.com>
2025-07-06 02:14:50 +0100

    Remocao da dependencia dj_database_url do settings,py

--- concessionaria/settings.py
+++ concessionaria/settings.py
@@ -4,7 +4,6 @@ import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
